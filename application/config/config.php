b4411617fef528123f36fd9fd27d9d30a8bbf129
Colin LEVERGER <colinleverger@gmail.com>
2016-03-03 13:10:34 +0100

    Update conf

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://experiment1.colinleverger.fr/';
+$config['base_url'] = 'http://srv-php-voeux.colinleverger.fr/';
 
 /*
 |--------------------------------------------------------------------------
